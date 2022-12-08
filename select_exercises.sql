4fff7a54052da064738f08389fbc65c8f618fbb9
Aldanis Vigo <aldanisvigo@gmail.com>
2022-12-08 13:18:30 -0600

    added captions to select_exercises

--- select_exercises.sql
+++ select_exercises.sql
@@ -6,22 +6,22 @@ USE codeup_test_db;
 -- In select_exercises.sql write queries to find the following information. Before each item, output a caption explaining the results:
 
 -- The name of all albums by Pink Floyd.
-SELECT name FROM albums WHERE artist = 'Pink Floyd';
+SELECT name AS 'All Pink Floyd Albums' FROM albums WHERE artist = 'Pink Floyd';
 
 -- The year Sgt. Pepper's Lonely Hearts Club Band was released
-SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+SELECT release_date AS 'Year Sgt. Pepper''s Lonely Hearts Club Band Was Released' FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
 -- The genre for Nevermind
-SELECT genre FROM albums WHERE name = 'Nevermind';
+SELECT genre AS 'Genre For Nirvana''s Nevermind' FROM albums WHERE name = 'Nevermind';
 
 -- Which albums were released in the 1990s
-SELECT * FROM albums WHERE release_date BETWEEN 1990 AND 1999;
+SELECT name 'Albums Released in The 1990s' FROM albums  WHERE release_date BETWEEN 1990 AND 1999;
 
 -- Which albums had less than 20 million certified sales
-SELECT * FROM albums WHERE sales < 20.0;
+SELECT name AS 'Albums With Less Than 20M Certified Sales' FROM albums WHERE sales < 20.0;
 
 -- All the albums with a genre of "Rock". 
-SELECT * FROM albums WHERE genre = 'Rock';
+SELECT name AS 'Rock Albums' FROM albums WHERE genre = 'Rock';
 
 -- Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
 -- Because we are checking for equality, if we wanted to see all the records with "rock" we would use LIKE and wildcards LIKE "%Rock%"
---
+++ update_exercises.sql
@@ -0,0 +1,22 @@
+-- Create a file called update_exercises.sql.
+USE codeup_test_db;
+
+-- After each SELECT add an UPDATE statement to:
+
+-- Make all the albums 10 times more popular (sales * 10)
+-- Move all the albums before 1980 back to the 1800s.
+-- Change 'Michael Jackson' to 'Peter Jackson'
+-- Add SELECT statements after each UPDATE so you can see the results of your handiwork.
+
+-- Write SELECT statements to output each of the following with a caption:
+
+-- All albums in your table.
+SELECT * FROM albums;
+
+-- All albums released before 1980
+SELECT * FROM albums WHERE release_date < 1980;
+
+-- All albums by Michael Jackson
+SELECT * FROM albums WHERE artist = 'Michael Jackson';
+
+
