38f5c3f9a4c9e3b56a502be483f1d6172b45ded2
Aldanis Vigo <aldanisvigo@gmail.com>
2022-12-10 09:22:14 -0600

    finished where_exercises.sql part one

---
+++ where_exercises.sql
@@ -0,0 +1,12 @@
+# Create a file named where_exercises.sql.
+# Make sure to use the employees database before you begin.
+USE employees;
+
+# Find all employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN).
+SELECT * FROM employees WHERE first_name IN ("Irena","Vidya","Maya");
+
+# Find all employees whose last name starts with 'E' — 7,330 rows.
+SELECT * FROM employees WHERE last_name LIKE "E%";
+
+# Find all employees with a 'q' in their last name — 1,873 rows.
+SELECT * FROM employees WHERE last_name LIKE "%q%";
